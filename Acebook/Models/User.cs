1c0c97a4d72bda1a5bf15c1c1392758d0e3b1a3c
Tom Wood <tomwood1875@gmail.com>
2025-02-12 15:52:13 +0000

    Add more tests to User class for friend request validation

--- Acebook/Models/User.cs
+++ Acebook/Models/User.cs
@@ -9,8 +9,9 @@ public class User
   public string? Email {get; set;}
   public string? Password {get; set;}
   public ICollection<Post>? Posts {get; set;}
-  public ICollection<User>? Friends {get; set;}
+  public ICollection<User>? Friends {get; set;}  // Created list to store all friends this user has added
 
+  // Constructor so we can create new users
   public User(string Name, string Email, string Password) 
   {
     this.Name = Name;
@@ -20,10 +21,26 @@ public class User
     this.Friends = new List<User>();
   }
 
-  public void AddFriend()
+  // Method to test adding another user as a friend
+  public string AddFriend(User friend)
   {
+    if (Friends.Contains(friend))
+    {
+        return "Error: This person is already a friend";
+    }
+    Friends.Add(friend);
+        return "Friend added successfully";
+  }    
 
+  // Method to test removing another user as a friend
+  public string RemoveFriend(User friend)
+  {
+      if (!Friends.Contains(friend))
+      {
+          return "Error: Cannot remove this person as they are not a friend";
+      }
+      Friends.Remove(friend);
+      return "Friend removed successfully";
   }
-
 
 }
