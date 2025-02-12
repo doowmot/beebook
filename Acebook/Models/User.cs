5b42f4008fcb8514f0226fe40b965731d40bb5b0
Lubica Cernakova <lubka.cernakova@gmail.com>
2025-02-12 21:13:47 +0000

    Settings page

--- Acebook/Models/User.cs
+++ Acebook/Models/User.cs
@@ -1,5 +1,6 @@
 namespace acebook.Models;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 
 public class User
 {
@@ -8,5 +9,8 @@ public class User
   public string? Name {get; set;}
   public string? Email {get; set;}
   public string? Password {get; set;}
+  public string? Profile_picture {get; set;}
   public ICollection<Post>? Posts {get; set;}
+
+  
 }
