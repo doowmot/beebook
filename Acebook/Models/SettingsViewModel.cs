8179dedabc031b09b62684fd99394c76b5fda221
Lubica Cernakova <lubka.cernakova@gmail.com>
2025-02-16 22:35:29 +0000

    display settings on page

---
+++ Acebook/Models/SettingsViewModel.cs
@@ -0,0 +1,8 @@
+namespace acebook.Models; // Make sure this matches your project namespace
+
+public class SettingsViewModel
+    {
+        public User User { get; set; }  // User profile data
+    }
+
+
