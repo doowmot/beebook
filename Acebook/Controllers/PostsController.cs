a8a4c89e1a180c3880db98a7a9e7ca375e58a11a
Ilhan Pamuk <ilhanpamukuk@gmail.com>
2025-02-20 14:28:23 +0000

    ordering of posts in orfile and posts

--- Acebook/Controllers/PostsController.cs
+++ Acebook/Controllers/PostsController.cs
@@ -17,15 +17,6 @@ public class PostsController : Controller
         _logger = logger;
     }
 
-    // [Route("/posts")]
-    // [HttpGet]
-    // public IActionResult Index() {
-    //   AcebookDbContext dbContext = new AcebookDbContext();
-    //   List<Post> posts = dbContext.Posts.ToList();
-    //   ViewBag.Posts = posts;
-    //   return View();
-    // }
-
   [Route("/posts")]
   [HttpGet]
   public IActionResult Index()
@@ -119,18 +110,6 @@ public class PostsController : Controller
       ViewBag.Comments = comments;
       return View();
     }
-
-    // [Route("/comment/create")]
-    // [HttpPost]
-    // public RedirectResult CommentCreate(int postId, Comment comment) 
-    // {
-    //   AcebookDbContext dbContext = new AcebookDbContext();
-    //   int currentUserId = HttpContext.Session.GetInt32("user_id").Value;
-    //   post.UserId = currentUserId;
-    //   dbContext.Posts.Add(comment);
-    //   dbContext.SaveChanges();
-    //   return new RedirectResult("/posts");
-    // }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
